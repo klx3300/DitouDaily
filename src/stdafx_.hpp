f49ce738ae94f4af11d41ec44121886a5abafc24
Recolic Rec <admin@recolic.net>
2016-10-28 09:25:13 +0800

    Update stdafx_.hpp

--- src/stdafx_.hpp
+++ src/stdafx_.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include <deque>
 #include <vector>
+#include <memory>
 #include "statements.hpp"
 #define VIRTUAL_MEMORY_SIZE 256
 
 extern deque<statement> buf;
-extern int *pMemory;
+extern shared_ptr<int> pMemory;
 extern vector<int> print_buffer;
 extern void __cdecl global_new_handler();
