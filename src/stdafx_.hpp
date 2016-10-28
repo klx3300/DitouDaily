12131741ed011e1c31e3fe9c77fa2a9999bff9de
Recolic Rec <admin@recolic.net>
2016-10-28 16:23:53 +0800

    Daily Update

--- src/stdafx_.hpp
+++ src/stdafx_.hpp
@@ -4,6 +4,11 @@
 #include <stack>
 #include "statements.hpp"
 
+#define cout
+#define wcout
+#define printf
+#define wprintf
+
 extern deque<statement> buf;
 extern vector<int> print_buffer;
 extern void __cdecl global_new_handler();
