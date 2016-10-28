200f37aa896bc1f141578758996abfc328d01354
imzhwk <inc19991@gmail.com>
2016-10-29 03:59:05 +0800

    analysis part II end.

--- src/analyser.cpp
+++ src/analyser.cpp
@@ -28,6 +28,7 @@ template <class T>
 class qLinkedList{
 	public:
 	qLinkedList();
+	qLinkedList(qLinkedList<T> src);
 	qNode<T>* first;
 	qNode<T>* last;
 	virtual T popfirst();
@@ -48,6 +49,13 @@ template<class T>
 qLinkedList<T>::qLinkedList(){
 	first=NULL;
 	last=NULL;
+}
+
+template<class T>
+qLinkedList<T>::qLinkedList(qLinkedList<T> src){
+	for(int i=0;src.get(i)!=NULL;i++){
+		addlast(src.get(i));
+	}
 }
 
 template<class T>
@@ -206,6 +214,7 @@ item::item(){
 	number=0;
 	oper=0;
 }
+//void bfcalc(qLinkedList<item> *exprlist,int start,int end,int lnnumber);
 
 void clearTempVarFlags(){
     for(int i<0;i<256;i++){
@@ -226,6 +235,17 @@ void freeTempVar(int i){
     tmpvars[i]=false;
 }
 
+
+int goto_level=-1;
+
+int allocGotoTag(){
+	return goto_level--;
+}
+
+void resetGotoTag(){
+	goto_level=-1;
+}
+
 void defineStatement(int ln,int varname){
 	string tmpstr("");
 	tmpstr+="int _____##@@";
@@ -239,6 +259,42 @@ void assignStatement(int ln,int varname,string simplest){
 	statement s(ln,tmpstr,S_ASSIGN);
 	buf.insert(currIndex++,s);
 }
+
+void fieldStatement(int ln,STATEMENT_T type){
+	statement s(ln,"",type);
+	buf.insert(currIndex++,s);
+}
+
+void gotoStatement(int ln){
+	statement s(ln,"",S_GOTO);
+	buf.insert(currIndex++,s);
+}
+
+void gotodestStatement(int ln){
+	statement s(ln,"",S_GOTO_DEST);
+	buf.insert(currIndex++,s);
+}
+
+void ifStatement(int ln,int varname){
+	statement s(ln,formatVarName(varname),S_IF);
+	buf.insert(currIndex++,s);
+}
+
+void elseStatement(int ln){
+	statement s(ln,"",S_ELSE);
+	buf.insert(currIndex++,s);
+}
+
+void printStatement(int ln){
+	statement s(ln,"",S_BLANK_BUT_OUTPUT);
+	buf.insert(currIndex++,s);
+};
+
+void breakStatement(int ln){
+	statement s(ln,"",S_BREAK);
+	buf.insert(currIndex++,s);
+};
+
 
 /* imzhwk created analyser
  * cmdType对应text中的内容如下：
@@ -280,7 +336,7 @@ string high_priority("*/");
 // operator 'n' indicates != operator
 
 int currIndex=0; // now analysis pointer position
-bool tmpvars[256];
+bool tmpvars[65536];
 
 //bracket-free part
 void bfcalc(qLinkedList<item> *exprlist,int start,int end,int lnnumber){
@@ -660,63 +716,9 @@ void bfcalc(qLinkedList<item> *exprlist,int start,int end,int lnnumber){
 		//printf("size:%d\n",exprlist->size());
 	}
 }
-// having the brackets part
-// operator D -> int
-// operator W -> while loop
-// operator F -> for loop
-// operator P -> printf
 
-void rcalc(qLinkedList<item> *exprlist){
-	/* possible sing_line expressions are:
-     * D varname = expression(or just close)
-     * W expression
-     * F expression ; expression ; expression
-     * P
-     * a=b like:not have startup operators
-     */
-    // detect startup operators
-    if(exprlist->first->item.type==TYPE_OPER){
-        
-    }else{
-        // that indicates it's an very simple assign statement(but may have brackets!).
-        bool havebrackets=true;
-        int firstplace=-1;
-        int lastplace=-1;
-        while(havebrackets){
-            //printstack(*exprlist);
-            //printf("size:%d\n",exprlist->size());
-			havebrackets=false;
-			//check brackets
-			for(int i=0;i<exprlist->size();i++){
-				if(exprlist->get(i)->item.type==TYPE_OPER and exprlist->get(i)->item.oper=='('){
-					havebrackets=true;
-					firstplace=i;
-				}
-				if(exprlist->get(i)->item.type==TYPE_OPER and exprlist->get(i)->item.oper==')'){
-					havebrackets=true;
-					lastplace=i;
-					break;
-				}
-			}
-			if(havebrackets==true){
-				//printf("have brackets:from %d to %d\n",firstplace,lastplace);
-				bfcalc(exprlist,firstplace,lastplace);
-				//printstack(*exprlist);
-				//printf("size:%d\n",exprlist->size());
-				firstplace=-1;lastplace=-1;
-			}
-		}
-    }
-    item lb,rb;
-	lb.type=TYPE_OPER;
-	lb.oper='(';
-	rb.type=TYPE_OPER;
-	rb.oper=')';
-	exprlist->addfirst(lb);
-	exprlist->addlast(rb);
-	bfcalc(exprlist,0,exprlist->size()-1);
-    
-    /* ------------------------orig code
+
+void debracket_process(qLinkedList<item> *exprlist,int ln){
 	bool havebrackets=true;
 	int firstplace=-1;
 	int lastplace=-1;
@@ -738,14 +740,15 @@ void rcalc(qLinkedList<item> *exprlist){
 		}
 		if(havebrackets==true){
 			//printf("have brackets:from %d to %d\n",firstplace,lastplace);
-			bfcalc(exprlist,firstplace,lastplace);
+			bfcalc(exprlist,firstplace,lastplace,ln);
 			//printstack(*exprlist);
 			//printf("size:%d\n",exprlist->size());
 			firstplace=-1;lastplace=-1;
 		}
 	}
-	// in order to fit in bfcalc() func
-	// surround simplest expression with brackets
+}
+
+void rebracket_process(qLinkedList<item> *exprlist,int ln){
 	item lb,rb;
 	lb.type=TYPE_OPER;
 	lb.oper='(';
@@ -753,10 +756,237 @@ void rcalc(qLinkedList<item> *exprlist){
 	rb.oper=')';
 	exprlist->addfirst(lb);
 	exprlist->addlast(rb);
-	bfcalc(exprlist,0,exprlist->size()-1);*/
+	bfcalc(exprlist,0,exprlist->size()-1,ln);
+}
+// having the brackets part
+// operator D -> int
+// operator J -> if
+// operator W -> while loop
+// operator F -> for loop
+// operator P -> printf
+// operator E -> else
+// operator B -> break
+// operator o -> do
+// when sent in,';' in the end of statement is removed!
+
+// true: didn't close normally(using '{' & '}')
+const int FIELD_NORMAL_TRUE=0,FIELD_NORMAL_FALSE=1,FIELD_LOOP_TRUE=2,FIELD_LOOP_FALSE=3,FIELD_DLOOP=4;
+
+qLinkedList<int> fieldStack;
+
+void rcalc(qLinkedList<item> *exprlist,int ln){
+	/* possible sing_line expressions are:
+     * D varname = expression(or just close)
+     * W expression
+     * F expression ; expression ; expression
+     * P
+	 * J expression
+     * a=b like:not have startup operators
+	 * } FIELD_END
+     */
+    // detect startup operators
+    if(exprlist->first->item.type==TYPE_OPER){
+		switch(exprlist->first->item.oper){
+			case 'D':
+				defineStatement(ln,exprlist->get(1)->item.number);
+				exprlist->popfirst();
+				debracket_process(exprlist);
+				rebracket_process(exprlist);
+				break;
+			case 'J':
+				exprlist->popfirst();
+				fieldStatement(ln,S_FIELD_BEGIN);
+				if(exprlist->last->item.oper=='{'){
+					exprlist->poplast();
+					debracket_process(exprlist);
+					rebracket_process(exprlist);
+					ifStatement(ln,exprlist->first->item.number);
+					fieldStack.addfirst(FIELD_NORMAL_FALSE);
+				}else{
+					debracket_process(exprlist);
+					rebracket_process(exprlist);
+					ifStatement(ln,exprlist->first->item.number);
+					fieldStack.addfirst(FIELD_NORMAL_TRUE);
+				}
+				break;
+			case 'W':
+				if(fieldStack.first->item!=FIELD_DLOOP){
+					// not a do-while loop!
+					exprlist->popfirst();
+					fieldStatement(ln,S_FIELD_BEGIN);
+					if(exprlist->last->item.oper=='{'){
+						exprlist->poplast();
+						gotodestStatement(ln);
+						debracket_process(exprlist);
+						rebracket_process(exprlist);
+						ifStatement(ln,exprlist->first->item.number);
+						fieldStack.addfirst(FIELD_LOOP_FALSE);
+					}else{
+						gotodestStatement(ln);
+						debracket_process(exprlist);
+						rebracket_process(exprlist);
+						ifStatement(ln,exprlist->first->item.number);
+						fieldStack.addfirst(FIELD_LOOP_TRUE);
+					}
+				}else{
+					// is a do-while loop!
+					fieldStack.popfirst();
+					exprlist->popfirst();
+					debracket_process(exprlist);
+					rebracket_process(exprlist);
+					ifStatement(ln,exprlist->first->item.number);
+					gotoStatement(ln);
+					fieldStatement(ln,S_FIELD_END);
+				}
+				break;
+			case 'F':
+				// divide the whole statement.
+				exprlist->popfirst();
+				qLinkedList<item> initexpr,condexpr,iterexpr;
+				for(int i=1;exprlist->get(i)->item.oper!=';';exprlist.popfirst()){
+					initexpr.addlast(exprlist->get(i)->item);
+				}
+				exprlist->popfirst();
+				for(int i=1;exprlist->get(i)->item.oper!=';';exprlist.popfirst()){
+					condexpr.addlast(exprlist->get(i)->item);
+				}
+				exprlist->popfirst();
+				for(int i=1;exprlist->get(i)->item.oper!=')';exprlist.popfirst()){
+					iterexpr.addlast(exprlist->get(i)->item);
+				}
+				// qLinkedList<item> cpcondexpr(condexpr);
+				// these statements divided suc.
+				fieldStatement(ln,S_FIELD_BEGIN);
+				if(exprlist->last->item.oper=='{'){
+					exprlist->poplast();
+					debracket_process(&initexpr);
+					rebracket_process(&initexpr);
+					assignStatement(ln,-998,"0");
+					gotodestStatement(ln);
+					fieldStatement(ln,S_FIELD_BEGIN);
+					ifStatement(ln,-998);
+					debracket_process(&iterexpr);
+					rebracket_process(&iterexpr);
+					fieldStatement(ln,S_FIELD_END);
+					assignStatement(ln,-998,"1");
+					debracket_process(&condexpr);
+					rebracket_process(&condexpr);
+					ifStatement(ln,condexpr.first->item.number);
+					fieldStack.addfirst(FIELD_LOOP_FALSE);
+				}else{
+					debracket_process(&initexpr);
+					rebracket_process(&initexpr);
+					assignStatement(ln,-998,"0");
+					gotodestStatement(ln);
+					fieldStatement(ln,S_FIELD_BEGIN);
+					ifStatement(ln,-998);
+					debracket_process(&iterexpr);
+					rebracket_process(&iterexpr);
+					fieldStatement(ln,S_FIELD_END);
+					assignStatement(ln,-998,"1");
+					debracket_process(&condexpr);
+					rebracket_process(&condexpr);
+					ifStatement(ln,condexpr.first->item.number);
+					fieldStack.addfirst(FIELD_LOOP_TRUE);
+				}
+				break;
+			case 'P':
+				printStatement(ln);
+				break;
+			case 'E':
+				elseStatement(ln);
+				fieldStatement(ln,S_FIELD_BEGIN);
+				fieldStack.addfirst(FIELD_NORMAL_TRUE);
+				break;
+			case 'B':
+				breakStatement(ln);
+				break;
+			case 'o':
+				fieldStack.addfirst(FIELD_DLOOP);
+				fieldStatement(ln,S_FIELD_BEGIN);
+				gotodestStatement(ln);
+				break;
+			case '{':
+				fieldStack.addfirst(FIELD_NORMAL_FALSE);
+				fieldStatement(ln,S_FIELD_BEGIN);
+				break;
+			case '}':
+					//close field
+					if(fieldStack.first->item==FIELD_NORMAL_FALSE){
+						// didn't close field normally!
+						// close it and pop the stack.
+						fieldStatement(ln,S_FIELD_END);
+						fieldStack.popfirst();
+						while(fieldStack.first->item==FIELD_NORMAL_TRUE){
+							fieldStatement(ln,S_FIELD_END);
+							fieldStack.popfirst();
+						}
+					}else if(fieldStack.first->item==FIELD_LOOP_FALSE){
+						// for loops
+						gotoStatement(ln);
+						fieldStatement(ln,S_FIELD_END);
+						fieldStack.popfirst();
+						while(fieldStack.first->item==FIELD_NORMAL_TRUE){
+							gotoStatement(ln);
+							fieldStatement(ln,S_FIELD_END);
+							fieldStack.popfirst();
+						}
+					}
+				break;
+			default:
+				// that's a statement that have no meanings!
+				// but we still have to execute it due to the fucking requirements!
+				debracket_process(exprlist);
+				rebracket_process(exprlist);
+				// check if last didn't use } to close field.
+				// true: didn't close normally(using '{' & '}')
+				// and commit close.
+				if(fieldStack.first->item==FIELD_NORMAL_TRUE){
+					// didn't close field normally!
+					// close it and pop the stack.
+					while(fieldStack.first->item==FIELD_NORMAL_TRUE){
+						fieldStatement(ln,S_FIELD_END);
+						fieldStack.popfirst();
+					}
+				}else if(fieldStack.first->item==FIELD_LOOP_TRUE){
+					// for loops
+					while(fieldStack.first->item==FIELD_NORMAL_TRUE){
+						gotoStatement(ln);
+						fieldStatement(ln,S_FIELD_END);
+						fieldStack.popfirst();
+					}
+				}
+				break;
+		}
+    }else{
+        // that indicates it's an very simple assign statement(but may have brackets!).
+        debracket_process(exprlist);
+		rebracket_process(exprlist);
+		// check if last didn't use } to close field.
+		// true: didn't close normally(using '{' & '}')
+		// and commit close.
+		if(fieldStack.first->item==FIELD_NORMAL_TRUE){
+			// didn't close field normally!
+			// close it and pop the stack.
+			while(fieldStack.first->item==FIELD_NORMAL_TRUE){
+				fieldStatement(ln,S_FIELD_END);
+				fieldStack.popfirst();
+			}
+		}else if(fieldStack.first->item==FIELD_LOOP_TRUE){
+			// for loops
+			while(fieldStack.first->item==FIELD_NORMAL_TRUE){
+				gotoStatement(ln);
+				fieldStatement(ln,S_FIELD_END);
+				fieldStack.popfirst();
+			}
+		}
+    }
+	
 }
 
 // when detected "for" loop, ignore next 2 ';'s (not to close statement too early.
+// when meet { or } , close statement immediately.the close result must include { or } itself.
+// when meet "else" , close statement immediately.
 
 
 int analyse_main(){
