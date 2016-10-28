b3d05bf8c67cad728b3cbd185bdc7bb9d1a9e9fb
Recolic Rec <admin@recolic.net>
2016-10-28 16:21:27 +0800

    Remove pre-allocated virtual memory

--- src/stdafx_.hpp
+++ src/stdafx_.hpp
@@ -2,11 +2,8 @@
 #include <deque>
 #include <vector>
 #include <stack>
-#include <memory>
 #include "statements.hpp"
-#define VIRTUAL_MEMORY_SIZE 256
 
 extern deque<statement> buf;
-extern shared_ptr<int> pMemory;
 extern vector<int> print_buffer;
 extern void __cdecl global_new_handler();
