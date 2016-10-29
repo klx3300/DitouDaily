8443137a18b175962b83e4ef559aebebac36291f
imzhwk <inc19991@gmail.com>
2016-10-29 16:05:51 +0800

    using namespace std;

--- src/stdafx_.hpp
+++ src/stdafx_.hpp
@@ -1,8 +1,10 @@
 #pragma once
 #include <deque>
 #include <vector>
+
 #include "statements.hpp"
 
+using namespace std;
 deque<statement> input_buf;//input to analyser.
 deque<statement> buf;//output from analyser.
 vector<int> print_buffer;
